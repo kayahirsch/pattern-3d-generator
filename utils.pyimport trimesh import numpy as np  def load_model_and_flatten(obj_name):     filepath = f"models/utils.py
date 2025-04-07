0cd0732c5070be2002d7ef183ccb10608bbce606
kayahirsch <126217291+kayahirsch@users.noreply.github.com>
2025-04-07 15:59:33 -0400

    Delete utils.pyimport trimesh import numpy as np  def load_model_and_flatten(obj_name):     filepath = f"models directory

--- utils.pyimport trimesh import numpy as np  def load_model_and_flatten(obj_name):     filepath = f"models/utils.py
+++
@@ -1,9 +0,0 @@
-import trimesh
-import numpy as np
-
-def load_model_and_flatten(obj_name):
-    filepath = f"models/{obj_name}.glb"
-    mesh = trimesh.load(filepath)
-    projection = mesh.vertices[:, :2]  # 3D to 2D (drop z-axis)
-    hull = trimesh.Trimesh(projection).convex_hull
-    return [(float(x), float(y)) for x, y in hull.vertices]
