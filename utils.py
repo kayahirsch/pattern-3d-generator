bc54489d5f3f7953918a83576cd9680a5f13037f
kayahirsch <126217291+kayahirsch@users.noreply.github.com>
2025-04-13 19:44:52 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ import numpy as np
 def load_model_and_flatten(obj_name):
     filepath = f"models/{obj_name}.glb"
     mesh = trimesh.load(filepath)
-    projection = mesh.vertices[:, :2]  # 3D to 2D (drop z-axis)
+    projection = mesh.vertices[:, :2]  # drop z-axis
     hull = trimesh.Trimesh(projection).convex_hull
     return [(float(x), float(y)) for x, y in hull.vertices]
